39552743600902842af34e02001264e93b9f05b4
Juho Junnila <jnnl.git@pm.me>
2019-10-17 17:32:32 +0300

    add redux-mock-store, use store for app.test

--- kged/src/tests/app.test.js
+++ kged/src/tests/app.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'components/app';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+import App from '../components/app'
+
+const mockStore = configureStore([])
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  const store = mockStore({
+      rooms: {
+          rooms: [],
+          activeRoom: {}
+      }
+  })
+
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div)
   ReactDOM.unmountComponentAtNode(div)
 })
