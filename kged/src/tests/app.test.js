d2561c3c884037e64570cb8802fea47b7e00f2ab
Samuli Helttunen <samuli.helttunen@gmail.com>
2019-10-17 19:36:34 +0300

    component Room testing with error (cannot read property toJSON of undefined)

--- kged/src/tests/app.test.js
+++ kged/src/tests/app.test.js
@@ -4,8 +4,32 @@ import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 
 import App from '../components/app'
+import Rooms from '../components/rooms'
 
 const mockStore = configureStore([])
+
+describe('Rooms', () => {
+  let store;
+  let component;
+
+  beforeEach(() =>{
+    store = mockStore({
+      myState: 'test',
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <Rooms />
+      </Provider>
+    );
+  });
+
+  it('should render with given state', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
