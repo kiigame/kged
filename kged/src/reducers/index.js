d37a099cc16690b02778c3e4471802ec1bb57eb3
Juho Junnila <jnnl.git@pm.me>
2019-10-17 19:01:21 +0300

    fix furniture reducer state in rootReducer

--- kged/src/reducers/index.js
+++ kged/src/reducers/index.js
@@ -5,7 +5,7 @@ import furnitures from './furnitures'
 const rootReducer = (state = {}, actions) => ({
     entity: entity(state.entity, actions, state),
     rooms: rooms(state.rooms, actions),
-    furnitures: furnitures(state.furniture, actions)
+    furnitures: furnitures(state.furnitures, actions)
 })
 
 export default rootReducer
