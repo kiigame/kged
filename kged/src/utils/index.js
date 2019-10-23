85ab56c7f0cfa881da81d162831703b18c77f87e
niko <niko.pelkonen@hotmail.com>
2019-10-24 00:30:08 +0300

    add validation for furniture name

--- kged/src/utils/index.js
+++ kged/src/utils/index.js
@@ -3,7 +3,10 @@ export function isExistingEntity(state, entityId) {
     if (state.rooms && state.rooms.rooms &&
         state.rooms.rooms.some(r => r.attrs && r.attrs.id === entityId)) {
         return true;
-    }
+    } if (state.furnitures && state.furnitures.furnitures &&
+        state.furnitures.furnitures.some(r => r.attrs && r.attrs.id === entityId)) {
+            return true;
+        }
     return false;
 }
 
