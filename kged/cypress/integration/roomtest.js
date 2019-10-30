2d1f556d4625cd763a259033cbd9607156b37562
Samuli Helttunen <gwchilos@hotmail.com>
2019-10-30 14:44:01 +0200

    cypress room creation tests

--- kged/cypress/integration/roomtest.js
+++ kged/cypress/integration/roomtest.js
@@ -6,4 +6,15 @@ describe('page visit', function() {
         cy.visit('https://kged-dev.netlify.com/')
         cy.get('button').should('have.class', 'm-2 col create-new-btn btn btn-success').click()
     })
+    it('gives the room a name and creates it', () => {
+        cy.visit('https://kged-dev.netlify.com/')
+        cy.get('button').should('have.class', 'm-2 col create-new-btn btn btn-success').click()
+        cy.get("[type='name']").type('huone123')
+        cy.get("[type='submit']").click()
+    })
+    it('cancels the room creation', () => {
+        cy.visit('https://kged-dev.netlify.com/')
+        cy.get('button').should('have.class', 'm-2 col create-new-btn btn btn-success').click()
+        cy.get("[type='button']").contains('Peruuta').click()
+    })
 })
