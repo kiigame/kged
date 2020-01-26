6131aa53e9d6c2463478e02a1ba9ca22bb57803a
Eino Keskitalo <eino.keskitalo@gmail.com>
2020-01-26 14:31:42 +0200

    Improve test description.

--- kged/src/components/__test__/app.test.js
+++ kged/src/components/__test__/app.test.js
@@ -3,7 +3,7 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import App from '../app'
 
-it('app is rendered', () => {
+it('app is rendered with expected structure', () => {
     const app = shallow(<App />);
     expect(app.find('div.app > div.row > div.pre-container').length).toEqual(1);
     expect(app.find('Sidebar').length).toEqual(1);
