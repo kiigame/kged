d96c432342a23d2e9713e78aaeb16a9bb98eeb44
Eino Keskitalo <eino.keskitalo@gmail.com>
2020-01-26 16:06:07 +0200

    Configure jest to use ensyme-to-json snapshort serializer, and create a snapshot for testing App component rendering.

--- kged/src/components/__test__/app.test.js
+++ kged/src/components/__test__/app.test.js
@@ -5,10 +5,5 @@ import App from '../app'
 
 it('app is rendered with expected structure', () => {
     const app = shallow(<App />);
-    expect(app.find('div.app > div.row > div.pre-container').length).toEqual(1);
-    expect(app.find('Sidebar').length).toEqual(1);
-    expect(app.find('div.pre-container > Connect(ActionBar)').length).toEqual(1);
-    expect(app.find('div.pre-container > Connect(Preview)').length).toEqual(1);
-    expect(app.find('div.pre-container > Connect(Console)').length).toEqual(1);
-    expect(app.find('Connect(Inspector)').length).toEqual(1);
+    expect(app).toMatchSnapshot();
 })
